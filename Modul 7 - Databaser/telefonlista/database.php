9d64a53d23547fabc071128fc57ed2287f6b63ce
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2017-11-20 14:18:09 +0100

    Modul 7 - Databaser

---
+++ Modul 7 - Databaser/telefonlista/database.php
@@ -0,0 +1,10 @@
+<?php
+$dbHost = "localhost"; 
+$dbUser = "mahmudalhakim";
+$dbPass = "TbHONG8qGAiplM58";
+$dbName = "telefonlista";
+
+$connection = 
+mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
+or die("Fel vid connect");
+?>
---
+++ Modul 7 - Databaser/telefonlista/insert.php
@@ -0,0 +1,17 @@
+<?php
+require('database.php');
+
+
+
+
+
+echo "<hr>---------------   Felsökning   ----------------------<br>";
+print ("<pre>My POST ");
+print_r($_POST);
+print ("</pre>");
+
+print ("<pre>My GET ");
+print_r($_GET);
+print ("</pre>");
+
+?>
