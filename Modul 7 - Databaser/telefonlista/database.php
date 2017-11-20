074c73fc1ec9d8670f88cb9a9eced6a033acdf25
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2017-11-20 16:02:00 +0100

    Modul 7 - Databaser

--- Modul 7 - Databaser/telefonlista/database.php
+++ Modul 7 - Databaser/telefonlista/database.php
@@ -7,4 +7,7 @@ $dbName = "telefonlista";
 $connection = 
 mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
 or die("Fel vid connect");
+
+// Lägg till denna rad för att lösa problem med svenska tecken.
+mysqli_set_charset($connection, "utf8");
 ?>
